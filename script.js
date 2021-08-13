f2c2f69836f6215b8a4b08d74c071e51801de471
Sam Sepiel <pojskicsafet@gmail.com>
2021-08-13 11:41:26 +0200

    Added intro animation, fixed overlapings and minor media query adjustments

---
+++ script.js
@@ -0,0 +1,25 @@
+/*Intro animation*/
+const intro = document.querySelector(".intro");
+const logo = document.querySelector(".logo-header");
+const logoSpan = document.querySelectorAll(".logo-i");
+
+window.addEventListener("DOMContentLoaded", () => {
+  logoSpan.forEach((span, idx) => {
+    setTimeout(() => {
+      span.classList.add("appear");
+    }, (idx + 1) * 90);
+  });
+
+  setTimeout(() => {
+    logoSpan.forEach((span, idx) => {
+      setTimeout(() => {
+        span.classList.remove("appear");
+        span.classList.add("fade");
+      }, (idx + 1) * 80);
+    });
+  }, 1500);
+
+  setTimeout(() => {
+    intro.style.top = "-100vh";
+  }, 2000);
+});
